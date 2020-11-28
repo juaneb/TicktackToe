0d413464b4bbbfd84c95ba0b3e05bb5ebc670cbb
juaneb <juan.escribano.bonilla@gmail.com>
2020-11-28 18:49:00 +0100

    MenuTest class and empty Menu class

---
+++ src/main/java/usantatecla/utils/Menu.java
@@ -0,0 +1,15 @@
+package usantatecla.utils;
+
+import usantatecla.tictactoe.views.Message;
+
+public class Menu {  
+
+  public Menu() {    
+  }
+
+  protected void execute(String[] comandos) {  
+        Console.getInstance().writeln("Aquí se van a pintar los comandos");        
+  }
+
+}
+
---
+++ src/test/java/usantatecla/utils/MenuTest.java
@@ -0,0 +1,45 @@
+package usantatecla.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+import org.mockito.MockedStatic;
+
+import usantatecla.tictactoe.views.Message;
+
+@ExtendWith(MockitoExtension.class)
+public class MenuTest {
+
+    private String[] comandos;
+
+    @InjectMocks
+    private Menu menu;
+
+    @Mock
+    private Console console;
+
+    @BeforeEach
+    void before() {
+        openMocks(this);
+    }
+
+    @Test
+    void testGivenArrayStringThenWriteOneString() {
+        try (MockedStatic console = mockStatic(Console.class)) {            
+            console.when(Console::getInstance).thenReturn(this.console);
+            menu.execute(comandos);            
+            verify(this.console, times(1)).writeln(anyString());           
+        }
+
+    }
+
+}
